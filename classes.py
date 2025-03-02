1bc34754e4833894e0d45f89e9f19aa5cf848711
Rattaphum Kaenmuang <66010704@KMITL.AC.TH>
2025-03-02 21:49:23 +0700

    Create superclass RDTEntity for common functionalities

--- classes.py
+++ classes.py
@@ -5,14 +5,21 @@ class Segment:
     def __init__(self, content, seqNum, ackNum):
         pass
 
-class RDTClient:
+class RDTEntity:
     def __init__(self):
+        self.seqNum = 0
+        self.ackNum = 0
+
+class RDTClient(RDTEntity):
+    def __init__(self):
+        super().__init__()
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.pairedServer = None #(serverIP, serverPort)
         
 
-class RDTServer:
+class RDTServer(RDTEntity):
     def __init__(self, ip, port):
+        super().__init__()
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.sock.bind((ip, port))
         self.sock.settimeout(SOCKET_TIMEOUT)
